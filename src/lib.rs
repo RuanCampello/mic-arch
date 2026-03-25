33300e52bc2384c3fa6e1fc752ba3797b49dcbbf
RuanCampello <ruancampello999@gmail.com>
2026-03-24 21:55:33 -0300

    test: add custom program test for the delivery

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,34 @@ pub mod logger;
 #[cfg(test)]
 #[allow(unused)]
 mod tests {
-    use crate::{cpu::Cpu, loader::load_program, logger::Logger};
+    use crate::{
+        cpu::{self, Cpu},
+        loader::load_program,
+        logger::Logger,
+    };
     use std::{fs::File, io::BufWriter, path::Path};
 
     #[test]
     fn output() -> Result<(), Box<dyn std::error::Error>> {
+        let a: u32 = 0b00000000000000000000000000001111;
+        let b: u32 = 0b00000000000000000000000011110000;
+
+        let input = Path::new("./program.txt");
+        let output = Path::new("./output.txt");
+
+        let mut cpu = Cpu::new();
+        let mut logger = Logger::new(File::create(&output)?);
+        logger.start_program(a, b)?;
+
+        let program = load_program(&input)?;
+        for (cycle, &instruction) in program.iter().enumerate() {
+            let result = cpu.execute_cycle(a, b, instruction);
+            logger.log_cycle(cycle + 1, &result)?;
+        }
+
+        logger.end_program(program.len() + 1)?;
+        println!("{}", std::fs::read_to_string(output)?);
+
         Ok(())
     }
 }
